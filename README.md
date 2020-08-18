b8a95acee5e1968da99928494fa9652b1fd730a5
Pablo Ocampo <pablojocampogo@gmail.com>
2020-08-17 22:31:20 -0400

    Add database tables creation scripts

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Repository of back end code for the Kargo Project
 
 ## Where can I find ... ?
  - Libraries used by many services: `/shared`
+ - Various scripts: `/scripts/<script-usage>`
  - Service code itself: `/<service-name>/functions`
  - Storage library used by a service: `/<service-name>/storage`
---
+++ scripts/dynamo-tables/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	dynamoClient dynamodbiface.DynamoDBAPI
+
+	tableConfigurations = []TableConfiguration{
+		TableConfiguration{
+			TableName:  "clients",
+			TableIndex: "client_id",
+		},
+		TableConfiguration{
+			TableName:  "partners",
+			TableIndex: "partner_id",
+		},
+		TableConfiguration{
+			TableName:  "orders",
+			TableIndex: "order_id",
+		},
+		TableConfiguration{
+			TableName:  "trips",
+			TableIndex: "trip_id",
+		},
+		TableConfiguration{
+			TableName:  "commodities",
+			TableIndex: "commodity_id",
+		},
+		TableConfiguration{
+			TableName:  "payment-accounts",
+			TableIndex: "payment_account_id",
+		},
+		TableConfiguration{
+			TableName:  "payment-transactions",
+			TableIndex: "payment_transaction_id",
+		},
+		TableConfiguration{
+			TableName:  "payment-methods",
+			TableIndex: "payment_method_id",
+		},
+	}
+)
+
+// TableConfiguration is struct handler for values necessary to create dynamo table
+type TableConfiguration struct {
+	TableName  string
+	TableIndex string
+}
+
+func init() {
+	sess := session.Must(session.NewSession())
+	dynamoClient = dynamodb.New(sess)
+}
+
+func createTable(tableConfiguration TableConfiguration) {
+	input := &dynamodb.CreateTableInput{
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String(tableConfiguration.TableIndex),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+		TableName: aws.String(tableConfiguration.TableIndex),
+	}
+
+	result, err := dynamoClient.CreateTable(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func main() {
+	for _, tableConfiguration := range tableConfigurations {
+		createTable(tableConfiguration)
+	}
+}
