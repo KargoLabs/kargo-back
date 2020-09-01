b7c5c2f9e139496ecb37af667c0396ee6728b53e
Pablo Ocampo <pablojocampogo@gmail.com>
2020-09-01 11:14:06 -0400

    Add gets without identity to the other service

---
+++ clients/functions/get-partner/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"kargo-back/shared/apigateway"
+	storage "kargo-back/storage/partners"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	errMissingPartnerID = errors.New("missing partner id in query parameter")
+)
+
+func apiGatewayHandler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	partnerID, ok := request.QueryStringParameters["partner_id"]
+
+	if !ok || partnerID == "" {
+		return apigateway.NewErrorResponse(400, errMissingPartnerID), nil
+	}
+
+	partner, err := storage.LoadPartner(ctx, partnerID)
+	if errors.Is(err, storage.ErrPartnerNotFound) {
+		return apigateway.NewErrorResponse(404, err), nil
+	}
+
+	if err != nil {
+		return apigateway.LogAndReturnError(err), nil
+	}
+
+	return apigateway.NewJSONResponse(200, partner), nil
+}
+
+func main() {
+	lambda.Start(apiGatewayHandler)
+}
---
+++ partners/functions/get-client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"kargo-back/shared/apigateway"
+	storage "kargo-back/storage/clients"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	errMissingClientID = errors.New("missing client id in query parameter")
+)
+
+func apiGatewayHandler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	clientID, ok := request.QueryStringParameters["client_id"]
+	if !ok || clientID == "" {
+		return apigateway.NewErrorResponse(400, errMissingClientID), nil
+	}
+
+	client, err := storage.LoadClient(ctx, clientID)
+	if errors.Is(err, storage.ErrClientNotFound) {
+		return apigateway.NewErrorResponse(404, err), nil
+	}
+
+	if err != nil {
+		return apigateway.LogAndReturnError(err), nil
+	}
+
+	return apigateway.NewJSONResponse(200, client), nil
+}
+
+func main() {
+	lambda.Start(apiGatewayHandler)
+}
