78033c80afa6f08ed6cbb2b706a9afa21746a680
Roniel Valdez <roniel_valdez@outlook.com>
2020-09-11 18:00:52 -0400

    complete model and add test to model

--- shared/transaction-models/transaction.go
+++ shared/transaction-models/transaction.go
@@ -20,12 +20,12 @@ var (
 	ErrInvalidAmount = errors.New("invalid amount parameter")
 )
 
-// PaymentStatus is the type handler for payment statuses.
-type PaymentStatus int
+// TransactionStatus is the type handler for payment TransactionStatus.
+type TransactionStatus int
 
 const (
 	// Started is when a trip has started
-	Started PaymentStatus = iota
+	Started TransactionStatus = iota
 	// InProgress is when a trip is in progress
 	InProgress
 	// Completed is when a trip has been completed
@@ -34,12 +34,12 @@ const (
 
 // Transaction is the struct handler for transaction
 type Transaction struct {
-	TransactionID string        `json:"transaction_id"`
-	PartnerID     string        `json:"partner_id"`
-	ClientID      string        `json:"client_id"`
-	Amount        int           `json:"amount"`
-	Status        PaymentStatus `json:"status"`
-	Date          time.Time     `json:"date"`
+	TransactionID string            `json:"transaction_id"`
+	PartnerID     string            `json:"partner_id"`
+	ClientID      string            `json:"client_id"`
+	Amount        int               `json:"amount"`
+	Status        TransactionStatus `json:"status"`
+	Date          time.Time         `json:"date"`
 }
 
 // NewTransaction returns a Transaction structure with given values
@@ -59,6 +59,11 @@ func NewTransaction(clientID, partnerID string, amount int) (*Transaction,
 	}
 
 	return &Transaction{
+		ClientID:  clientID,
+		PartnerID: partnerID,
+		Amount:    amount,
+		Status:    Started,
+		Date:      time.Now(),
 		TransactionID: random.GetSHA256WithPrefix(TransactionIDPrefix,
 			time.Now().String()),
 	}, nil
